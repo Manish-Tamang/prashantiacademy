c3a45fd916a5a716017a48e52d0dd0cbb6af35e9
Manish Tamang <126256480+Manishtmang@users.noreply.github.com>
2023-12-21 11:27:38 +0545

    Update page.jsx

--- src/app/News/page.jsx
+++ src/app/News/page.jsx
@@ -5,7 +5,6 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import { createClient } from "@sanity/client";
 import OnePost from "@/components/OnePost"; // Import your OnePost component here
-import "@/styles/style.css";
 
 const client = createClient({
   projectId: "uewhm6v9",
@@ -44,9 +43,7 @@ const AllPosts = () => {
 
   if (loading) {
     return (
-      <div className="dot-spinner">
-        {/* Add your loading animation here */}
-      </div>
+      <div>Loading</div>
     );
   }
 
@@ -56,45 +53,35 @@ const AllPosts = () => {
         {/* Route to the individual post */}
         <Route element={<OnePost />} path="/:slug" exact />
       </Routes>
-      
       <div className="bg-black-100 min-h-screen p-12">
-        <div className="container mx-auto">
-          <h2 className="text-5xl text-gray-700 flex justify-center">
-            News & events
-          </h2>
-          <h3 className="text-lg text-gray-600 flex justify-center mb-12">
-            Welcome to News & Events of Prashanti Academy
-          </h3>
-          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {allPostsData.length > 0 ? (
-              allPostsData.map((post, index) => (
-                // Link to individual post using react-router-dom Link
-                <Link
-                  to={`/${post.slug.current}`} // Use 'to' attribute for react-router-dom Link
-                  key={post.slug.current}
-                >
-                  <span
-                    className="bg-white block h-64 relative rounded shadow leading-snug border-l-8 border-green-400"
-                    key={index}
-                  >
-                    <img
-                      className="w-full h-full rounded-r object-cover absolute"
-                      src={post.mainImage?.asset?.url || ""}
-                      alt={post.title}
-                    />
-                    <span className="block relative h-full flex justify-end items-end pr-4 pb-4"></span>
-                    <h2 className="text-gray-800 text-lg font-bold px-3 py-4 bg-gray-700 text-red-100 bg-opacity rounded">
-                      {post.title}
-                    </h2>
-                  </span>
-                </Link>
-              ))
-            ) : (
-              <p>No posts found.</p> // Display a message if there are no posts
-            )}
-          </div>
-        </div>
-      </div>
+  <div className="container mx-auto">
+    <h2 className="text-5xl text-gray-700 text-center mb-6">News & Events</h2>
+    <h3 className="text-lg text-gray-600 text-center mb-12">
+      Welcome to News & Events of Prashanti Academy
+    </h3>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      {allPostsData.length > 0 ? (
+        allPostsData.map((post, index) => (
+          <Link to={`/${post.slug.current}`} key={post.slug.current}>
+            <div className="relative bg-white rounded shadow border-l-8 border-green-400 overflow-hidden">
+              <img
+                className="w-full h-48 md:h-64 object-cover"
+                src={post.mainImage?.asset?.url || ""}
+                alt={post.title}
+              />
+              <div className="absolute inset-0 bg-gray-1000 bg-opacity-0  opacity-1000  flex flex-col justify-end p-4">
+                <h2 className="text-gray-100 text-lg font-bold">{post.title}</h2>
+              </div>
+            </div>
+          </Link>
+        ))
+      ) : (
+        <p className="text-white text-center">No posts found.</p>
+      )}
+    </div>
+  </div>
+</div>
+
     </Router>
   );
 };
