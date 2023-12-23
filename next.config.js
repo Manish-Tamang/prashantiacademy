09d0a8db3d4038cb256f7f9650acaa18938622d8
Manish Tamang <126256480+Manishtmang@users.noreply.github.com>
2023-12-23 14:13:55 +0545

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['img.icons8.com'], // Add the hostname of your images here
-      // Add other domains if your application uses images from multiple sources
-    },
+    
   };
   
   module.exports = nextConfig;
