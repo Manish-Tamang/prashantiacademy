279873b4438b85d0e93c9c950bc5ed41e5d19fe5
Manish Tamang <126256480+Manishtmang@users.noreply.github.com>
2024-01-31 21:10:35 +0545

    update

--- src/app/developer/page.jsx
+++ src/app/developer/page.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import PageIntro from "@/components/PageIntro";
 import Dev from "@/components/dev";
+import Testimonials from "@/components/NotFound";
+import Container from "@/components/Container";
+import Manish from "../images/Dev/Untitled.png";
 
 const Developer = () => {
   return (
     <>
-{/*       <img src={Manish} alt="Manish Tamang" /> */}
+ <Container className="flex h-full items-center pt-20 sm:pt-24 lg:pt-32">
+       <Testimonials
+      className="mt-24 sm:mt-32 lg:mt-40"
+      client={{
+        name: "Image",
+        logo: Manish
+      }}
+      style={{ borderRadius: "4px" }}
+    >
+ I think you're lost
+      </Testimonials>
       <PageIntro eyebrow="Developer" title="मनिष तामाङ">
         <p>
           Manish Tamang is a student of <strong>Prashanti Academy</strong>{" "}
@@ -13,6 +26,7 @@ const Developer = () => {
         </p>
         <Dev />
       </PageIntro>
+      </Container>
     </>
   );
 };
