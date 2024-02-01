6bb9c619f0e9c1d369aa0b33ead8d4bf110f90b9
Manish Tamang <126256480+Manishtmang@users.noreply.github.com>
2024-02-01 23:15:04 +0545

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+// next.config.js
+module.exports = {
+  images: {
+    domains: ['www.facebook.com'], // Replace 'your-domain.com' with your actual domain
+  },
+};
